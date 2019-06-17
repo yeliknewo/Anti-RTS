089ad86c2c7c750a6c57d7854fa117af9f776bc9
vgamerone <lindelandj@wit.edu>
2019-06-17 10:34:38 -0400

    Added sprites, Player movement and shooting

---
+++ Anti-RTS/Assets/Base.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    //Chunk chunk
+    public int resources;
+    //Planner planner
+    public int health;
+    public int maxHealth;
+    //public miningTarget;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
