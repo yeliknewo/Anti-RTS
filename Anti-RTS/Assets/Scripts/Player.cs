dc81a35f0709c7f2a3685f87a975eb100c6be15d
vgamerone <lindelandj@wit.edu>
2019-07-09 15:33:07 -0400

    Changed Player script for joystick controls

--- Anti-RTS/Assets/Scripts/Player.cs
+++ Anti-RTS/Assets/Scripts/Player.cs
@@ -30,7 +30,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate((float)movementSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, (float)movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+        //transform.Translate((float)movementSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, (float)movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+
+        Vector3 movement = new Vector3(Input.GetAxis("HorizontalLeft") * (float)movementSpeed * Time.deltaTime, Input.GetAxis("VerticalLeft") * (float)movementSpeed * Time.deltaTime);
+        rb2d.MovePosition(transform.position + movement);
+
+        float angle = (Mathf.Atan2(Input.GetAxis("VerticalRight"), Input.GetAxis("HorizontalRight")) * Mathf.Rad2Deg) - 90;
+        Debug.Log(angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        
         //Mathf.atan2 to get the angle of an x,y cos give x, sin gives y.
         //rotation = quaternian.euler of atan2 to handle player rotation.
         //Get camera to follow player
@@ -44,7 +52,7 @@ public class Player : MonoBehaviour
             transform.Rotate(0f, 0f, -1f);
             //playerShooter.transform.localRotation = transform.rotation;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Fire1"))
         {
             GameObject bulletShot = Instantiate(bullet,transform.position,transform.rotation);
             bulletShot.transform.position = playerShooter.position;
