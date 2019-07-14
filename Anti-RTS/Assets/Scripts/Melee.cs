b080a031bdd8a66cf005a39e415413905548be1e
Chris Ligonde <ligondec@wit.edu>
2019-07-14 17:09:05 -0400

    Pushing ENUMS and Identifier component

---
+++ Anti-RTS/Assets/Scripts/Melee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+    Enemy enemy
+    {
+        get {
+            return this.gameObject.GetComponent<Enemy>();
+        }
+    }
+    
+    int damage;
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Enemy>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
